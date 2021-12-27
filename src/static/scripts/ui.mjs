5d6847d64a392aefaef20cd881122c2801985b73
Ashur Cabrera <hello@ashur.cab>
2021-12-26 21:56:22 -0700

    Implement modal functions

--- src/static/scripts/ui.mjs
+++ src/static/scripts/ui.mjs
@@ -1,8 +1,37 @@
 let UI = {
+	hideModal()
+	{
+		document.querySelector( ".sb-modal-system" ).classList.remove( "sb-modal-open" );
+		document.querySelector( ".sb-modal-wrapper" ).innerHTML = "";
+	},
+
 	showModal: ({title, subtitle, body} = {}) =>
 	{
-		console.log( "Show modal", {title}, {subtitle}, {body} );
-	}
+		let modalHeader = `<h3 class="sb-modal-title">${title}</h3>`;
+		if( subtitle )
+		{
+			modalHeader += `<h4 class="sb-modal-subtitle">${subtitle}</h4>`;
+		}
+
+		let modalWrapper = `<div role="button" class="sb-modal-frame help">
+			<div class="sb-modal-top">
+				<div role="button" class="sb-modal-close">×</div>
+			</div>
+
+			<div class="sb-modal-content">
+				<div class="sb-modal-header">
+					${modalHeader}
+				</div>
+				<div class="sb-modal-body">
+					${body}
+				</div>
+			</div>
+		</div>`
+
+		document.querySelector( ".sb-modal-system" ).classList.add( "sb-modal-open" );
+		document.querySelector( ".sb-modal-wrapper" ).innerHTML = modalWrapper;
+		document.querySelector( ".sb-modal-close" ).addEventListener( "click", this.hideModal );
+	},
 }
 
 export default UI;
