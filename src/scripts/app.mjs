c1bf852db731caf4705006f86880411437500a71
Ashur Cabrera <hello@ashur.cab>
2021-12-27 21:37:10 -0700

    Update App

--- src/scripts/app.mjs
+++ src/scripts/app.mjs
@@ -11,18 +11,71 @@ class App
 		this.grid = grid;
 	}
 
-	async fetchGrid( url )
+	/**
+	 * @param {string} url
+	 * @return {Object}
+	 */
+	static async fetchGrid( url )
 	{
+		let gridUrl = new URL( url );
+		let today = gridUrl.pathname
+			.split( "/" )
+			.slice( 1, 4 )
+			.join( "-" );
+
+		let localData = App.getLocalData();
+
+		// We already fetched today's grid
+		if( localData.today === today )
+		{
+			return localData.grid;
+		}
+
 		try
 		{
-			let html = await fetch( url );
-			console.log( html );
+			let response = await fetch( url );
+			let div = document.createElement( "html" );
+			div.innerHTML = await response.text();
 
-			return html;
+			let gridElement = div.querySelector( ".StoryBodyCompanionColumn p:nth-child(5)" );
+			if( gridElement )
+			{
+				localData.grid = App.parseGrid( gridElement.innerText );
+				localData.today = today;
+
+				App.setLocalData( localData );
+			}
+			else
+			{
+				throw new Error( "There was a problem fetching today’s grid" );
+			}
 		}
 		catch( error )
 		{
-			// ...
+			if( error.isSpellCheckError )
+			{
+				console.log( error.details );
+			}
+
+			throw new Error( "There was a problem parsing today’s grid" );
+		}
+
+		return localData.grid;
+	}
+
+	/**
+	 * @returns {Object}
+	 */
+	static getLocalData()
+	{
+		let localData = localStorage.getItem( "spellcheck" );
+		if( localData )
+		{
+			return JSON.parse( localData );
+		}
+		else
+		{
+			return {};
 		}
 	}
 
@@ -113,13 +166,106 @@ class App
 		return result;
 	}
 
-	showModal()
+	/**
+	 * @param {Object} data
+	 */
+	static setLocalData( data )
 	{
-		let body = "";
+		localStorage.setItem( "spellcheck", JSON.stringify( data ) );
+	}
+
+	/**
+	 * @param {string} errorMessage
+	 * @param {string} [details] - ex., .message from a thrown Error
+	 */
+	static showErrorModal( errorMessage, details )
+	{
+		let body = errorMessage
+			.split( "\n" )
+			.map( line => `<p class="sb-modal-message">${line}</p>` )
+			.join( "\n" );
+
+		body += `<p class="sb-modal-message">Please click or tap the Spell Check bookmarklet to try again, or <a href="#">report the issue</a>.</p>`;
+
+		if( details )
+		{
+			body += `<details class=""><summary>More details</summary><span class="sc-details">${details}</span></details>`;
+		}
 
 		UI.showModal({
 			title: "Spell Check",
-			subtitle: "Remaining words in today’s puzzle.",
+			subtitle: "An unexpected problem occurred.",
+			body: body,
+		});
+	}
+
+	/**
+	 * Show the grid modal
+	 */
+	showGridModal()
+	{
+		let {words} = JSON.parse( localStorage.getItem( "sb-today" ) );
+		let gridRemaining = this.gridRemaining( words );
+
+		let wordLengths = this.grid.wordLengths
+			.map( wordLength =>
+			{
+				return `<th>${wordLength}</th>`
+			})
+			.join( "\n" );
+
+		let wordLengthRow = `
+			<tr>
+				<th></th>
+				${wordLengths}
+			</tr>`;
+
+		let distributionRows = Object.keys( gridRemaining.distributions )
+			.map( letter =>
+			{
+				let distributionRow = `
+				<tr>
+					<th>${letter}</th>`;
+
+				Object.keys( gridRemaining.distributions[letter] ).forEach( wordLength =>
+				{
+					let classList = [];
+					let originalValue = this.grid.distributions[letter][wordLength]
+					let currentValue = gridRemaining.distributions[letter][wordLength];
+
+					if( currentValue === 0 )
+					{
+						if( originalValue === 0 )
+						{
+							currentValue = "-";
+						}
+						else
+						{
+							currentValue = '<span class="checked"><span class="visually-hidden">0</span></span>';
+						}
+					}
+
+					distributionRow += `<td class="${classList.join( " " )}">${currentValue}</td>`;
+				});
+
+				distributionRow += "</tr>";
+
+				return distributionRow;
+			})
+			.join( "\n" );
+
+		let body = `
+			<table class="sb-modal-table">
+				<tbody>
+					${wordLengthRow}
+					${distributionRows}
+				</tbody>
+			</table>
+		`;
+
+		UI.showModal({
+			title: "Spell Check",
+			subtitle: "Words remaining in today’s puzzle.",
 			body: body,
 		});
 	}
