1504db4720f028e4cf00d56f5dd67cf207b33420
Ashur Cabrera <hello@ashur.cab>
2021-12-28 08:34:07 -0700

    Update bookmarklet

--- src/scripts/bookmarklet.mjs
+++ src/scripts/bookmarklet.mjs
@@ -5,14 +5,6 @@ import UI from "./ui.mjs";
 {
 	if( !window.spellcheck )
 	{
-		let gridStatic = `		  4	  5	  6	  7	  8	  9	  Σ
-A:	  3	  4	  2	  1	  -	  -	 10
-G:	  4	  3	  2	  2	  -	  -	 11
-L:	  3	  1	  -	  -	  1	  -	  5
-M:    7	  8	  3	  -	  -	  1	 19
-R:	  2	  1	  -	  -	  -	  -	  3
-Σ:	 19	 17	  7	  3	  1	  1	 48`;
-
 		try
 		{
 			UI.addStyles();
@@ -25,10 +17,10 @@ R:	  2	  1	  -	  -	  -	  -	  3
 				window.spellcheckApp.showErrorModal( "Spell Check was unable to locate today’s grid.", `Could not locate "Today’s Hints" menu item` );
 			}
 
-			let grid = await App.fetchGrid( hintsLink.href );
-
+			console.log( "⬇️ Fetching today’s grid" );
+			let remoteGrid = await App.fetchGrid( hintsLink.href );
 			window.spellcheckApp = new App({
-				grid: grid
+				grid: remoteGrid
 			});
 		}
 		catch( error )
