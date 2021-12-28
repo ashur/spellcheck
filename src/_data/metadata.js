01d2f093f34c4e895f07b37908dfc811219941bc
Ashur Cabrera <hello@ashur.cab>
2021-12-28 12:52:34 -0700

    Add metadata data

---
+++ src/_data/metadata.js
@@ -0,0 +1,9 @@
+module.exports = {
+	env: {
+		url: process.env.URL || "http://localhost:8080",
+	},
+	site: {
+		title: "Spell Check",
+		description: "Check your Spelling Bee progress without leaving the puzzle",
+	},
+}
