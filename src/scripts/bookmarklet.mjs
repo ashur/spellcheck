5e6d3e138a8e80a98a6fb2df43453124f6f3f39c
Ashur Cabrera <hello@ashur.cab>
2021-12-27 21:37:32 -0700

    Update bookmarklet script

--- src/scripts/bookmarklet.mjs
+++ src/scripts/bookmarklet.mjs
@@ -1,28 +1,48 @@
 import App from "./app.mjs";
 import UI from "./ui.mjs";
 
-if( !window.spellcheck )
+(async () =>
 {
-	let grid = `         4	  5	  6	  7	  8	  9	 11	  Σ
-C:	  1	  2	  -	  1	  -	  -	  -	 4
-E:	  -	  1	  2	  1	  -	  -	  -	  4
-I:	  -	  1	  4	  1	  -	  3	  1	 10
-N:	  3	  1	  -	  1	  1	  -	  -	  6
-T:	  2	  1	  -	  -	  -	  -	  -	  3
-V:	  3	  -	  -	  -	  -	  -	  -	 3
-Z:	  3	  -	  -	  -	  -	  -	  -	  3
-Σ:	 12	  6	  6	  4	  1	  3	  1	 33`;
+	if( !window.spellcheck )
+	{
+		let gridStatic = `		  4	  5	  6	  7	  8	  9	  Σ
+A:	  3	  4	  2	  1	  -	  -	 10
+G:	  4	  3	  2	  2	  -	  -	 11
+L:	  3	  1	  -	  -	  1	  -	  5
+M:    7	  8	  3	  -	  -	  1	 19
+R:	  2	  1	  -	  -	  -	  -	  3
+Σ:	 19	 17	  7	  3	  1	  1	 48`;
 
-	UI.addStyles();
+		try
+		{
+			UI.addStyles();
 
-	window.spellcheckApp = new App({
-		grid: grid
-	});
-}
+			let hintsLink = document.querySelector( ".pz-toolbar-button__hints" )
+				|| document.querySelector( ".pz-dropdown__menu-item[href]" )
 
-if( !window.spellcheckUi )
-{
-	window.spellcheckUi = UI;
-}
+			if( !hintsLink || !hintsLink.href )
+			{
+				window.spellcheckApp.showErrorModal( "Spell Check was unable to locate today’s grid.", `Could not locate "Today’s Hints" menu item` );
+			}
 
-window.spellcheckApp.showModal();
+			let grid = await App.fetchGrid( hintsLink.href );
+
+			window.spellcheckApp = new App({
+				grid: grid
+			});
+		}
+		catch( error )
+		{
+			console.log( error );
+
+			App.showErrorModal( "Spell Check was unable to fetch today’s grid.", error.message );
+		}
+	}
+
+	if( !window.spellcheckUi )
+	{
+		window.spellcheckUi = UI;
+	}
+
+	window.spellcheckApp.showGridModal();
+})();
