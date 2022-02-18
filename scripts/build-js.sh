61523f83d471d5e77f0a3dd36baf0a68edaba295
Ashur Cabrera <hello@ashur.cab>
2022-02-17 22:25:32 -0700

    Switch to $DEPLOY_PRIME_URL
    $URL is returning the production site URL (https://spellcheck.fun) even on branch deploys

--- scripts/build-js.sh
+++ scripts/build-js.sh
@@ -1,12 +1,14 @@
+SPELLCHECK_URL=$DEPLOY_PRIME_URL
+
 echo "⚙️  Build settings..."
 echo
 echo "  DEBUG=$DEBUG"
-echo "  URL=$URL"
+echo "  SPELLCHECK_URL=$SPELLCHECK_URL"
 
 echo
 echo "☕️ Building script..."
-npx esbuild src/scripts/init.mjs --bundle --outfile=dist/init.js --define:SPELLCHECK_URL="\"$URL\"" --define:SPELLCHECK_DEBUG="\"$DEBUG\""
+npx esbuild src/scripts/init.mjs --bundle --outfile=dist/init.js --define:SPELLCHECK_URL="\"$SPELLCHECK_URL\"" --define:SPELLCHECK_DEBUG="\"$DEBUG\""
 
 echo
 echo "☕️ Building minified script..."
-npx esbuild src/scripts/init.mjs --bundle --minify --sourcemap --outfile=dist/init.min.js --define:SPELLCHECK_URL="\"$URL\"" --define:SPELLCHECK_DEBUG="\"$DEBUG\""
+npx esbuild src/scripts/init.mjs --bundle --minify --sourcemap --outfile=dist/init.min.js --define:SPELLCHECK_URL="\"$SPELLCHECK_URL\"" --define:SPELLCHECK_DEBUG="\"$DEBUG\""
